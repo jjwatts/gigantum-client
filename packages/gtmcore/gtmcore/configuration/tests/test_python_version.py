f6bf1ae5d7cf3db5ece86ebe0dfe7645d694750f
Bill Van Besien <bill@gigantum.io>
2019-02-05 11:55:44 -0500

    Change circleci container
    Signed-off-by: Bill Van Besien <bill@gigantum.io>

--- packages/gtmcore/gtmcore/configuration/tests/test_python_version.py
+++ packages/gtmcore/gtmcore/configuration/tests/test_python_version.py
@@ -1,8 +1,14 @@
 import pytest
 import platform
 
+from dataclasses import dataclass
+
 
 class TestPythonVersion(object):
     def test_python_version(self):
+        @dataclass
+        class TestDataClass(object):
+            x: int = 2
+            
         python_version = platform.python_version()
         assert python_version.startswith('3.7')
